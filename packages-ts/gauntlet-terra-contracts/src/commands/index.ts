3b958bc426d6726e8adfcc83878968bae165ec24
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2022-01-04 14:06:48 +0100

    removed proxy command

--- packages-ts/gauntlet-terra-contracts/src/commands/index.ts
+++ packages-ts/gauntlet-terra-contracts/src/commands/index.ts
@@ -2,6 +2,5 @@ import Upload from './tooling/upload'
 import TransferLink from './contracts/link/transfer'
 import DeployLink from './contracts/link/deploy'
 import OCR2 from './contracts/ocr2'
-import ProxyOCR2 from './contracts/proxyOcr2'
 
-export default [Upload, DeployLink, TransferLink, ...OCR2, ...ProxyOCR2]
+export default [Upload, DeployLink, TransferLink, ...OCR2]
