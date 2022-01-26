6902c3b81ccd891165212296b27d3bca82699b3a
connorwstein <connor.stein@mail.mcgill.ca>
2022-01-26 18:43:59 -0500

    Back to 1.5

--- pkg/terra/config.go
+++ pkg/terra/config.go
@@ -16,7 +16,12 @@ var DefaultConfigSet = ConfigSet{
 	BlocksUntilTxTimeout:  10,
 	ConfirmPollPeriod:     time.Second,
 	FallbackGasPriceULuna: sdk.MustNewDecFromStr("0.015"),
-	GasLimitMultiplier:    1.2,
+	// This is high since we simulate before signing the transaction.
+	// There's a chicken and egg problem: need to sign to simulate accurately
+	// but you need to specify a gas limit when signing.
+	// TODO: Determine how much gas a signature adds and then
+	// add that directly so we can be more accurate.
+	GasLimitMultiplier:    1.5,
 	// The max gas limit per block is 1_000_000_000
 	// https://github.com/terra-money/core/blob/d6037b9a12c8bf6b09fe861c8ad93456aac5eebb/app/legacy/migrate.go#L69.
 	// The max msg size is 10KB https://github.com/terra-money/core/blob/d6037b9a12c8bf6b09fe861c8ad93456aac5eebb/x/wasm/types/params.go#L15.
