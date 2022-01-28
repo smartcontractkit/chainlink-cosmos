8a9893b4afbd2f81ba4f9b18d16f390aa3a48513
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2022-01-28 12:54:56 -0800

    Add some useful scripts, update README.md

--- packages-ts/gauntlet-terra-contracts/src/lib/constants.ts
+++ packages-ts/gauntlet-terra-contracts/src/lib/constants.ts
@@ -15,16 +15,19 @@ export const DEFAULT_RELEASE_VERSION = 'v0.0.4'
 export const ORACLES_MAX_LENGTH = 31
 
 export const CW20_BASE_CODE_IDs = {
-  mainnet: 3,
+  'mainnet': 3,
+  'local': 32,
   'bombay-testnet': 148
 }
 
 export const CW4_GROUP_CODE_IDs = {
-  mainnet: -1,
+  'mainnet': -1,
+  'local': -1,
   'bombay-testnet' : 36895
 }
 
 export const CW3_FLEX_MULTISIG_CODE_IDs = {
-  mainnet: -1,
+  'mainnet': -1,
+  'local': -1,
   'bombay-testnet' : 36059
 }
