de1ec9485b52180da3da507a934fd671902ac9a0
Blaž Hrastnik <blaz@mxxn.io>
2022-02-07 17:56:13 +0900

    Refactor setConfig into a multi-step proposal process

--- contracts/ocr2/tests/integration.rs
+++ contracts/ocr2/tests/integration.rs
@@ -83,6 +83,12 @@ fn init_works() {
         transmitters: transmitters.clone(),
         f,
         onchain_config: Binary(vec![]),
+    };
+
+    let execute_info = mock_info(OWNER, &[]);
+    let response: Response = execute(&mut deps, mock_env(), execute_info, msg).unwrap();
+
+    let msg = ExecuteMsg::SetOffchainConfig {
         offchain_config_version: 1,
         offchain_config: Binary(vec![4, 5, 6]),
     };
