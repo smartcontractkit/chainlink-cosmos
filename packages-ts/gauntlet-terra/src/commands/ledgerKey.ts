1f127ea518e2b327a05ec9e67799e7f8cbed6bc2
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2022-02-08 13:22:30 +0100

    27989: adding secp256k1 to parse the signature

--- packages-ts/gauntlet-terra/src/commands/ledgerKey.ts
+++ packages-ts/gauntlet-terra/src/commands/ledgerKey.ts
@@ -2,6 +2,7 @@ import { Key, SimplePublicKey } from '@terra-money/terra.js'
 import LedgerTerraConnector, {ERROR_CODE} from '@terra-money/ledger-terra-js'
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid"
 import { logger } from '@chainlink/gauntlet-core/dist/utils'
+import { signatureImport } from 'secp256k1';
 
 
 export class LedgerKey extends Key {
@@ -38,18 +39,19 @@ export class LedgerKey extends Key {
     }
 
     public async sign(payload: Buffer): Promise<Buffer> {
+        const {ledgerConnector, terminateConnection} = await this.connectToLedger()
         try { 
-            const {ledgerConnector} = await this.connectToLedger()
-
             const response = await ledgerConnector.sign(this.path, payload)
             if (response.return_code !== ERROR_CODE.NoError) {
                 throw new Error(response.error_message)
             }
 
-            return Buffer.from(response.signature.data)
+            return Buffer.from(signatureImport(Buffer.from(response.signature as any)));
         } catch (e) {
             logger.error(`LedgerKey sign failed: ${e.message}. Is Ledger unlocked and Terra app open?`)
             throw e
+        } finally {
+            await terminateConnection()
         }
     }
 
