42f553d082a10ff7efdd72e10962d56e8871235f
Tate <tate.exon@smartcontract.com>
2022-02-09 09:46:17 -0700

    Fix the ci issue with the network file being writting to an invalid directory for gauntlet

--- tests/e2e/smoke/gauntlet_test.go
+++ tests/e2e/smoke/gauntlet_test.go
@@ -11,6 +11,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/smartcontractkit/chainlink-terra/tests/e2e"
 	"github.com/smartcontractkit/chainlink-terra/tests/e2e/common"
+	"github.com/smartcontractkit/chainlink-terra/tests/e2e/utils"
 	"github.com/smartcontractkit/helmenv/environment"
 	"github.com/smartcontractkit/helmenv/tools"
 	"github.com/smartcontractkit/integrations-framework/actions"
@@ -20,12 +21,13 @@ import (
 
 var _ = Describe("Terra Gauntlet @gauntlet", func() {
 	var (
-		e     *environment.Environment
-		g     *gauntlet.Gauntlet
-		nodes []client.Chainlink
-		nets  *client.Networks
-		nkb   []common.NodeKeysBundle
-		err   error
+		e              *environment.Environment
+		g              *gauntlet.Gauntlet
+		nodes          []client.Chainlink
+		nets           *client.Networks
+		nkb            []common.NodeKeysBundle
+		err            error
+		networkDirPath string
 	)
 
 	terraCommandError := "Terra Command execution error"
@@ -60,6 +62,7 @@ var _ = Describe("Terra Gauntlet @gauntlet", func() {
 			Expect(err).ShouldNot(HaveOccurred())
 		})
 		By("Setup Gauntlet", func() {
+			networkDirPath = filepath.Join(utils.ProjectRoot, "./packages-ts/gauntlet-terra-contracts/networks")
 			_, err := exec.LookPath("yarn")
 			Expect(err).ShouldNot(HaveOccurred())
 
@@ -78,7 +81,7 @@ var _ = Describe("Terra Gauntlet @gauntlet", func() {
 			terraNodeUrl, err := e.Charts.Connections("localterra").LocalURLByPort("lcd", environment.HTTP)
 			Expect(err).ShouldNot(HaveOccurred())
 			g.NetworkConfig = common.GetDefaultGauntletConfig(terraNodeUrl)
-			err = g.WriteNetworkConfigMap()
+			err = g.WriteNetworkConfigMap(networkDirPath)
 			Expect(err).ShouldNot(HaveOccurred(), "failed to write the .env file")
 		})
 	})
