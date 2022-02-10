c65725e92c177f59b24f89ca3d2785597d18f313
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-02-10 13:38:57 +0000

    add test for the txresults source to make sure it counts failed txs correctly

---
+++ pkg/monitoring/source_txresults_test.go
@@ -0,0 +1,44 @@
+package monitoring
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
+	"github.com/smartcontractkit/chainlink/core/logger"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTxResultsSource(t *testing.T) {
+	rawTxs, err := os.ReadFile("./fixtures/txs.json")
+	require.NoError(t, err, "should be able to read fixtures")
+
+	t.Run("should correcly count failed and succeeded transactions", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, _ *http.Request) {
+			_, err := writer.Write(rawTxs)
+			require.NoError(t, err)
+		}))
+		defer srv.Close()
+
+		chainConfig := generateChainConfig()
+		chainConfig.FCDURL = srv.URL
+		feedConfig := generateFeedConfig()
+
+		factory := NewTxResultsSourceFactory(logger.NullLogger)
+		source, err := factory.NewSource(chainConfig, feedConfig)
+		require.NoError(t, err)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+		data, err := source.Fetch(ctx)
+		require.NoError(t, err)
+		txResults, ok := data.(relayMonitoring.TxResults)
+		require.True(t, ok)
+		require.Equal(t, uint64(94), txResults.NumSucceeded)
+		require.Equal(t, uint64(6), txResults.NumFailed)
+	})
+}
