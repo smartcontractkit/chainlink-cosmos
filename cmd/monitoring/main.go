da486fdc3c2dced8a3549c64696aa556f37f2432
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2022-02-15 11:31:03 +0100

    Fix monitoring on-chain config

--- cmd/monitoring/main.go
+++ cmd/monitoring/main.go
@@ -1,54 +1,37 @@
 package main
 
 import (
-	"context"
+	// "context"
 
-	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
-	"github.com/smartcontractkit/chainlink-terra/pkg/monitoring"
+	// relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
+	// "github.com/smartcontractkit/chainlink-terra/pkg/monitoring"
 	"github.com/smartcontractkit/chainlink/core/logger"
 )
 
 func main() {
-	ctx := context.Background()
+	// ctx := context.Background()
 
 	log := logger.NewLogger().With("project", "terra")
 
-	terraConfig, err := monitoring.ParseTerraConfig()
-	if err != nil {
-		log.Fatalw("failed to parse terra specific configuration", "error", err)
-	}
+	// terraConfig, err := monitoring.ParseTerraConfig()
+	// if err != nil {
+	// 	log.Fatalw("failed to parse terra specific configuration", "error", err)
+	// }
 
-	terraSourceFactory, err := monitoring.NewTerraSourceFactory(
-		terraConfig,
-		log.With("component", "source"),
-	)
-	if err != nil {
-		log.Fatalw("failed to initialize Terra source", "error", err)
-	}
+	// terraSourceFactory, err := monitoring.NewTerraSourceFactory(terraConfig, log.With("component", "source"))
+	// if err != nil {
+	// 	log.Fatalw("failed to initialize Terra source", "error", err)
+	// }
 
-	relayMonitoring.Entrypoint(
-		ctx,
-		logWrapper{log},
-		terraConfig,
-		terraSourceFactory,
-		monitoring.TerraFeedParser,
-		[]relayMonitoring.SourceFactory{},
-		[]relayMonitoring.ExporterFactory{},
-	)
+	// relayMonitoring.Entrypoint(
+	// 	ctx,
+	// 	log,
+	// 	terraConfig,
+	// 	terraSourceFactory,
+	// 	monitoring.TerraFeedParser,
+	// 	[]relayMonitoring.SourceFactory{},
+	// 	[]relayMonitoring.ExporterFactory{},
+	// )
 
 	log.Info("monitor stopped")
-}
-
-type logWrapper struct {
-	logger.Logger
-}
-
-var _ relayMonitoring.Logger = logWrapper{}
-
-func (l logWrapper) Criticalw(format string, values ...interface{}) {
-	l.Logger.CriticalW(format, values...)
-}
-
-func (l logWrapper) With(values ...interface{}) relayMonitoring.Logger {
-	return logWrapper{l.Logger.With(values...)}
 }
