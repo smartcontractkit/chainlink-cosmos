b8420b873f2c0802c07f8a27666e256b16418740
Frank Zhu <fr@nkzhu.com>
2022-02-23 14:04:50 -0600

    update for propose and confirm proxy

--- packages-ts/gauntlet-terra-contracts/src/commands/contracts/proxy_ocr2/proposeContract.ts
+++ packages-ts/gauntlet-terra-contracts/src/commands/contracts/proxy_ocr2/proposeContract.ts
@@ -3,6 +3,7 @@ import { AccAddress } from '@terra-money/terra.js'
 import { CATEGORIES } from '../../../lib/constants'
 import { CONTRACT_LIST } from '../../../lib/contracts'
 import { AbstractInstruction, instructionToCommand } from '../../abstract/executionWrapper'
+import { getRDD } from '../../../lib/rdd'
 
 type CommandInput = {
   address: string
@@ -13,8 +14,12 @@ type ContractInput = {
 }
 
 const makeCommandInput = async (flags: any, args: string[]): Promise<CommandInput> => {
+  const rdd = getRDD(flags.rdd)
+  const contract = args[0]
+  const aggregator = rdd.contracts[contract]
+
   return {
-    address: flags.address,
+    address: aggregator,
   }
 }
 
