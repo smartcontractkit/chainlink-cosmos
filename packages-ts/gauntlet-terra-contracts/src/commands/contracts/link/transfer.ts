13f040019c2bb66842092526649049026bf13177
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2022-03-02 12:51:03 -0800

    Add -batch=JSONFILE option to token:transfer commmand
    transfers.json:
      [
        { "to": "terra14ghwwjnfmg7w4fssgrp7czw3f4z0g93zav2w0m",
          "amount": 5.000000000000001
        },
        { "to": "terra10natxaqh5wfzags45nc0mv5zmrq0x87pzvg9w7",
          "amount": 4.500
        }
      ]
    
    Usage:
    
    > yarn gauntlet token:transfer --network=testnet-bombay --link=terra14p3g2kwmpdl4nrm8vva8wf68dt98w0ly0kgq4h --batch=transfers
    🧤  gauntlet 0.0.7
    ℹ️   Operator address is terra1pl4k5rj2jv6phm2vvhkttju7px6va2ja2v3haw
    ℹ️   About to Send 5.000000000000001 LINK (5000000000000001000) to terra14ghwwjnfmg7w4fssgrp7czw3f4z0g93zav2w0m.
    ℹ️   About to Send 4.5 LINK (4500000000000000000) to terra10natxaqh5wfzags45nc0mv5zmrq0x87pzvg9w7.
    🤔  Send all? (Y / N)y
    ✅  LINK transfers were successful (txhash: 99BA41703392B79DB3575C8CEED9E5AAA866F4E0DF8DA161E6C25792BF16627F)

--- packages-ts/gauntlet-terra-contracts/src/commands/contracts/link/transfer.ts
+++ packages-ts/gauntlet-terra-contracts/src/commands/contracts/link/transfer.ts
@@ -2,6 +2,15 @@ import { TerraCommand, TransactionResponse } from '@chainlink/gauntlet-terra'
 import { Result } from '@chainlink/gauntlet-core'
 import { BN, logger, prompt } from '@chainlink/gauntlet-core/dist/utils'
 import { CATEGORIES } from '../../../lib/constants'
+import { io } from '@chainlink/gauntlet-core/dist/utils'
+import { assert } from 'console'
+
+interface LinkContractInput {
+  transfer: {
+    recipient: string
+    amount: number
+  }
+}
 
 export default class TransferLink extends TerraCommand {
   static description = 'Transfer LINK'
@@ -12,6 +21,15 @@ export default class TransferLink extends TerraCommand {
 
   static id = 'token:transfer'
   static category = CATEGORIES.LINK
+  static flags = {
+    decimals: { description: 'Digits after decimal point to include in amount' },
+    amount: { description: 'Amount of LINK to transfer, as a decimal number' },
+    to: { description: 'Address of destination wallet' },
+    batch: {
+      description:
+        'Name of JSON file with a list of transfers to execute at once:  [ { "to" : ADDRESS, "amount": AMOUNT }, ... ]',
+    },
+  }
 
   constructor(flags, args: string[]) {
     super(flags, args)
@@ -21,19 +39,63 @@ export default class TransferLink extends TerraCommand {
     throw new Error('Transfer LINK command: makeRawTransaction method not implemented')
   }
 
-  execute = async () => {
-    const decimals = this.flags.decimals || 18
-    const link = this.flags.link || process.env.LINK
-    const amount = new BN(this.flags.amount).mul(new BN(10).pow(new BN(decimals)))
+  validateBatchInputs = (jsonFile, inputs) => {
+    if (!Array.isArray(inputs)) {
+      throw Error(`Invalid contents of ${jsonFile}, must be a list: ${inputs}`)
+    } else if (inputs.length == 0) {
+      throw Error(`Read empty list from ${jsonFile}, aborting.`)
+    }
+  }
 
-    await prompt(`Sending ${this.flags.amount} LINK (${amount.toString()}) to ${this.flags.to}. Continue?`)
-    const tx = await this.call(link, {
+  makeContractInput = (flags): LinkContractInput => {
+    const decimals = this.flags.decimals || 18
+    let amount
+    // Do we really want to round this to nearest integer, ignoring everything after decimal?
+    //const makeAmount = (amount) => new BN(amount).mul(new BN(10).pow(new BN(decimals)))
+    try {
+      amount = Number.parseFloat(flags.amount) * Math.pow(10, decimals)
+    } catch {
+      throw Error(`Failed to parse float ${flags.amount}`)
+    }
+    return {
       transfer: {
-        recipient: this.flags.to,
+        recipient: flags.to,
         amount: amount.toString(),
       },
-    })
-    logger.success(`LINK transferred successfully to ${this.flags.to} (txhash: ${tx.hash})`)
+    } as LinkContractInput
+  }
+
+  confirmBatchSend = async (jsonInputs, inputs) => {
+    assert(jsonInputs.length == inputs.length, 'Length mismatch')
+    for (let i = 0; i < inputs.length; i++) {
+      logger.info(
+        `About to Send ${jsonInputs[i].amount} LINK (${inputs[i].transfer.amount}) to ${inputs[i].transfer.recipient}.`,
+      )
+    }
+    await prompt(`Send all?`)
+  }
+
+  execute = async () => {
+    const link = this.flags.link || process.env.LINK
+    const batchFile: string = this.flags.batch
+    let tx
+
+    if (this.flags.batch) {
+      const jsonInputs = io.readJSON(batchFile)
+      this.validateBatchInputs(this.flags.batch, jsonInputs)
+      const inputs: LinkContractInput[] = jsonInputs.map(this.makeContractInput)
+      await this.confirmBatchSend(jsonInputs, inputs)
+      tx = await this.callBatch(this.flags.link, inputs)
+      logger.success(`LINK transfers were successful (txhash: ${tx.hash})`)
+    } else {
+      const contractInput = this.makeContractInput(this.flags)
+
+      await prompt(
+        `Sending ${this.flags.amount} LINK (${contractInput.transfer.amount}) to ${this.flags.to}. Continue?`,
+      )
+      tx = await this.call(link, contractInput)
+      logger.success(`LINK transferred successfully to ${this.flags.to} (txhash: ${tx.hash})`)
+    }
     return {
       responses: [
         {
--- packages-ts/gauntlet-terra/src/commands/internal/terra.ts
+++ packages-ts/gauntlet-terra/src/commands/internal/terra.ts
@@ -98,6 +98,22 @@ export default abstract class TerraCommand extends WriteCommand<TransactionRespo
     return this.wrapResponse(res)
   }
 
+  async callBatch(address, inputs) {
+    const msgs: MsgExecuteContract[] = inputs.map(
+      (input) => new MsgExecuteContract(this.wallet.key.accAddress, address, input),
+    )
+
+    const tx = await this.wallet.createAndSignTx({
+      msgs: msgs,
+      ...(this.wallet.key instanceof LedgerKey && {
+        signMode: SignMode.SIGN_MODE_LEGACY_AMINO_JSON,
+      }),
+    })
+
+    const res = await this.provider.tx.broadcast(tx)
+    return this.wrapResponse(res)
+  }
+
   async deploy(codeId, instantiateMsg, migrationContract = undefined) {
     const instantiate = new MsgInstantiateContract(
       this.wallet.key.accAddress,
