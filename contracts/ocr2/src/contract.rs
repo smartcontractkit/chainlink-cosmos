9234c54c86ae195a1a01a842d8d3ee2306eab503
connorwstein <connor.stein@mail.mcgill.ca>
2022-03-04 12:39:22 -0500

    Sanity check reimbursement calc

--- contracts/ocr2/src/contract.rs
+++ contracts/ocr2/src/contract.rs
@@ -1536,6 +1536,7 @@ pub(crate) mod tests {
         mock_dependencies, mock_env, mock_info, MockApi, MockQuerier, MockStorage,
     };
     use cosmwasm_std::OwnedDeps;
+    use crate::Decimal;
 
     fn setup() -> OwnedDeps<MockStorage, MockApi, MockQuerier> {
         let mut deps = mock_dependencies(&[]);
@@ -1707,5 +1708,25 @@ pub(crate) mod tests {
         };
         let execute_info = mock_info("payee2", &[]);
         execute(deps.as_mut(), mock_env(), execute_info, msg).unwrap();
+    }
+
+    #[test]
+    fn test_calculate_reimbursement() {
+        use std::str::FromStr;
+        let recommended_gas_price = Decimal::from_str("0.011000").unwrap();
+        let juels_per_fee_coin = u128::from_str("6000000000000000000").unwrap(); // 6e18 juels in 1 luna (i.e. 6 link)
+
+        // Sanity check
+        let r = calculate_reimbursement(&Billing{
+            recommended_gas_price_micro: recommended_gas_price,
+            observation_payment_gjuels: 0,
+            transmission_payment_gjuels: 0,
+            gas_base: Some(84_000),
+            gas_per_signature: Some(17_000),
+            gas_adjustment: Some(140),
+        }, juels_per_fee_coin, 1);
+        // juels = ((gas_per_sig*sigcount + gas_base)*gas_price_uluna/1e6)*juels_per_fee_coin
+        // juels = ((1 * 17000 + 84000)*1.4*0.011/1e6)*6e18 = 9332400000000000
+        assert_eq!(Uint128::from_str("9332400000000000").unwrap(), r);
     }
 }
