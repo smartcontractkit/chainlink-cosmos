a1913ed4553be7f7bfc5c8309fe17cafd1900f77
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2022-03-04 13:07:22 +0100

    small refactor

--- packages-ts/gauntlet-terra-contracts/src/lib/inspection.ts
+++ packages-ts/gauntlet-terra-contracts/src/lib/inspection.ts
@@ -1,10 +1,11 @@
 import { Proto } from '@chainlink/gauntlet-core/dist/crypto'
 import { BN } from '@chainlink/gauntlet-core/dist/utils'
 
-export const toComparableNumber = (v: string | number) => new BN(v).toString()
 export const toComparableLongNumber = (v: Long) => new BN(Proto.Protobuf.longToString(v)).toString()
-export const wrappedComparableLongNumber = (v: any) => {
+
+export const toComparableNumber = (v: string | number | Long) => {
   // Proto encoding will ignore falsy values.
   if (!v) return '0'
+  if (typeof v === 'string' || typeof v === 'number') return new BN(v).toString()
   return toComparableLongNumber(v)
 }
