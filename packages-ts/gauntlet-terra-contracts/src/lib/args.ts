7bf64ca9526997acea05e89351f24ad4a56dc94f
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2022-03-08 08:44:33 +0100

    Fix CI by removing a non-working 'defaultFlags.rdd'

--- packages-ts/gauntlet-terra-contracts/src/lib/args.ts
+++ packages-ts/gauntlet-terra-contracts/src/lib/args.ts
@@ -2,5 +2,7 @@ export const defaultFlags = {
   delta: 'delta.json',
   codeIdsPath: './codeIds',
   artifactsPath: './artifacts',
-  rdd: '../reference-data-directory/directory-terra-mainnet.json',
+  // TODO: when enabled it will overwrite --rdd flag always, not just when -rdd flag missing
+  // Default path is set as next to chainlink-terra repo folder
+  // rdd: '../../../../../reference-data-directory/directory-${network}.json',
 }
