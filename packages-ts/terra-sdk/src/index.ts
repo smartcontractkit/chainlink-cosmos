6e71296af4987e0adacd01e9fab06844e2d64089
Jordan Krage <jmank88@gmail.com>
2022-03-09 07:47:52 -0600

    add aggregator_round_id and observations_timestamp to Round

--- packages-ts/terra-sdk/src/index.ts
+++ packages-ts/terra-sdk/src/index.ts
@@ -1,6 +1,14 @@
 import { WebSocketClient } from '@terra-money/terra.js';
 import { TxLog, Int } from '@terra-money/terra.js';
 
+export interface Round {
+    answer: Int;
+    round: number;
+    epoch: number;
+    aggregatorRoundId: number;
+    observationsTS: Date;
+}
+
 export class OCR2Feed {
     private _wsClient: WebSocketClient;
 
@@ -35,21 +43,29 @@ export class OCR2Feed {
     private static roundFromAttributes(attrs: {[k:string]:string[]}): Round {
         let onlyAttr = (key:string): string => {
             let vals = attrs[key];
-            if (vals.length != 1) {
+            if (!vals || vals.length != 1) {
                 return null;
             }
             return vals[0];
         }
+        let tryInt = (s?:string): number => {
+            return parseInt(s) || null;
+        }
+        let tryBig = (s?:string): Int => {
+            if (!s) return null;
+            return new Int(s);
+        }
+        let tryUnixDate = (s?:string): Date => {
+            let unixTS = tryInt(s);
+            if (!unixTS) return null;
+            return new Date(unixTS * 1000);
+        }
         return {
-            answer: new Int(onlyAttr("answer")),
-            round: parseInt(onlyAttr("round")),
-            epoch: parseInt(onlyAttr("epoch")),
+            answer: tryBig(onlyAttr("answer")),
+            round: tryInt(onlyAttr("round")),
+            epoch: tryInt(onlyAttr("epoch")),
+            aggregatorRoundId: tryInt(onlyAttr("aggregator_round_id")),
+            observationsTS: tryUnixDate(onlyAttr("observations_timestamp")),
         }
     }
-}
-
-export interface Round {
-    answer: Int;
-    round: number;
-    epoch: number;
 }
--- packages-ts/terra-sdk/test/index.test.ts
+++ packages-ts/terra-sdk/test/index.test.ts
@@ -8,23 +8,40 @@ describe('OCR2Feed', () => {
             answer:new Int(9099065386),
             round:6,
             epoch:26787,
+            aggregatorRoundId: 25016,
+            observationsTS: new Date("2022-03-04T17:40:23.000Z"),
         }]);
     })
     it('parseLog multi big and negative', () => {
         let got = OCR2Feed.parseLog('[' +
-            '{"events":[{"type":"wasm-new_transmission","attributes":[{"key":"answer","value":"99999999999999999999999999999999"},{"key":"epoch","value":"13"},{"key":"round","value":"42"}]}]},' +
-            '{"events":[{"type":"wasm-new_transmission","attributes":[{"key":"answer","value":"-1234567890"},{"key":"epoch","value":"42"},{"key":"round","value":"13"}]}]}' +
+            '{"events":[{"type":"wasm-new_transmission","attributes":[{"key":"answer","value":"99999999999999999999999999999999"},{"key":"epoch","value":"13"},{"key":"round","value":"42"},{"key":"observations_timestamp","value":"1646417623"},{"key":"aggregator_round_id","value":"6688"}]}]},' +
+            '{"events":[{"type":"wasm-new_transmission","attributes":[{"key":"answer","value":"-1234567890"},{"key":"epoch","value":"42"},{"key":"round","value":"13"},{"key":"aggregator_round_id","value":"99"},{"key":"observations_timestamp","value":"1646425623"}]}]}' +
             ']');
         expect(got).toEqual([{
             answer:new Int('99999999999999999999999999999999'),
             round:42,
             epoch:13,
+            aggregatorRoundId: 6688,
+            observationsTS: new Date("2022-03-04T18:13:43.000Z"),
         },{
             answer:new Int(-1234567890),
             round:13,
             epoch:42,
+            aggregatorRoundId: 99,
+            observationsTS: new Date("2022-03-04T20:27:03.000Z"),
         }]);
     })
+    it('parseLogEmpty', () => {
+        let got = OCR2Feed.parseLog('[{"events":[{"type":"execute_contract","attributes":[{"key":"sender","value":"terra167h3sh8c4pgs8grxz24pam2x764flydv3h9pd8"},{"key":"contract_address","value":"terra1fwhxcdlm7cefu585pv54vgcpacq9daxppa823c"}]},{"type":"from_contract","attributes":[{"key":"contract_address","value":"terra1fwhxcdlm7cefu585pv54vgcpacq9daxppa823c"},{"key":"method","value":"transmit"},{"key":"method","value":"transmit"}]},{"type":"message","attributes":[{"key":"action","value":"/terra.wasm.v1beta1.MsgExecuteContract"},{"key":"module","value":"wasm"},{"key":"sender","value":"terra167h3sh8c4pgs8grxz24pam2x764flydv3h9pd8"}]},{"type":"wasm","attributes":[{"key":"contract_address","value":"terra1fwhxcdlm7cefu585pv54vgcpacq9daxppa823c"},{"key":"method","value":"transmit"},{"key":"method","value":"transmit"}]},{"type":"wasm-new_transmission","attributes":[{"key":"contract_address","value":"terra1fwhxcdlm7cefu585pv54vgcpacq9daxppa823c"},{"key":"transmitter","value":"terra167h3sh8c4pgs8grxz24pam2x764flydv3h9pd8"},{"key":"observers","value":"0d0c020805010409060b0e0003070000000000000000000000000000000000"},{"key":"juels_per_fee_coin","value":"156135087"},{"key":"config_digest","value":"000283aa7440f0e70a44e5e122a6a4112b4670c9a72f8746eeda1cc93c296b1a"},{"key":"reimbursement","value":"460650"},{"key":"observations","value":"9094516578"},{"key":"observations","value":"9094516578"},{"key":"observations","value":"9094516578"}]},{"type":"wasm-transmitted","attributes":[{"key":"contract_address","value":"terra1fwhxcdlm7cefu585pv54vgcpacq9daxppa823c"},{"key":"config_digest","value":"000283aa7440f0e70a44e5e122a6a4112b4670c9a72f8746eeda1cc93c296b1a"}]}]}]');
+        expect(got).toEqual([{
+            answer:null,
+            round:null,
+            epoch: null,
+            aggregatorRoundId:null,
+            observationsTS:null,
+        }]);
+    })
+    // requires a WS_URL for Columbus-5
     it('onRound', async () => {
         let cl = new OCR2Feed(new WebSocketClient(process.env.WS_URL));
         let resolve;
@@ -40,5 +57,7 @@ describe('OCR2Feed', () => {
         expect(got.answer).toBeDefined();
         expect(got.round).toBeDefined();
         expect(got.epoch).toBeDefined();
+        expect(got.aggregatorRoundId).toBeDefined();
+        expect(got.observationsTS).toBeDefined();
     }, 120_000)
 })
