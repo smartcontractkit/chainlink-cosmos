1beaa24a57f1984f2b89ffcbc3227ccbaab68bdb
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2022-03-09 16:23:33 +0100

    Referencing issue and workaround message

--- packages-ts/gauntlet-terra-contracts/src/lib/args.ts
+++ packages-ts/gauntlet-terra-contracts/src/lib/args.ts
@@ -1,3 +1,5 @@
+// Doesn't work as expected - https://github.com/smartcontractkit/chainlink-terra/issues/199
+// As a workaround, default RDD flag is set from env in gauntlet-terra/src/lib/rdd.ts
 export const defaultFlags = {
   delta: 'delta.json',
   codeIdsPath: './codeIds',
