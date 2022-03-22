e5770c7b601a73ffe2cedb6de88e70c096d03a0b
Akhil Chainani <akhil.chainani@smartcontract.com>
2022-03-22 09:27:29 -0400

    Remove excess print statement
    Co-authored-by: Kristijan Rebernisak <kristijan.rebernisak@gmail.com>

--- ops/deployer/gauntlet/gauntlet.go
+++ ops/deployer/gauntlet/gauntlet.go
@@ -124,7 +124,6 @@ func (t *GauntlerDeployer) DeployLINK() error {
 	}
 
 	report, err := t.gauntlet.ReadCommandReport()
-	fmt.Println(report)
 	if err != nil {
 		fmt.Println(err)
 		return errors.New("report not available")
