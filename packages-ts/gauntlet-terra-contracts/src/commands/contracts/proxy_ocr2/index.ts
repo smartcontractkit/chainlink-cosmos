a85481df751614ab409585dcc61f1c3362edaf33
Dimitris Grigoriou <dimitris.riou@gmail.com>
2022-03-25 14:40:43 +0200

    Add inspect command for proxy_ocr2 (#234)
    * Add inspect command for proxy_ocr2
    
    * Remove version from inspection
    
    * Imporve latest round data implementation
    
    * Imporve inspect logs
    
    * Include decimals to inspection

--- packages-ts/gauntlet-terra-contracts/src/commands/contracts/proxy_ocr2/index.ts
+++ packages-ts/gauntlet-terra-contracts/src/commands/contracts/proxy_ocr2/index.ts
@@ -3,11 +3,13 @@ import ProposeContract from './proposeContract'
 import ConfirmContract from './confirmContract'
 import { makeTransferOwnershipCommand, makeAcceptOwnershipCommand } from '../ownership'
 import { CONTRACT_LIST } from '../../../lib/contracts'
+import Inspect from './inspection/inspect'
 
 export default [
   Deploy,
   ProposeContract,
   ConfirmContract,
+  Inspect,
   makeTransferOwnershipCommand(CONTRACT_LIST.PROXY_OCR_2),
   makeAcceptOwnershipCommand(CONTRACT_LIST.PROXY_OCR_2),
 ]
