56425aa7db66b12368455833676c3398f0d1a84e
Akhil Chainani <akhil.chainani@smartcontract.com>
2022-05-02 11:16:57 -0400

    upgrade gauntlet-core + refactor logger

--- packages-ts/gauntlet-terra/src/commands/logger.ts
+++ packages-ts/gauntlet-terra/src/commands/logger.ts
@@ -1,38 +1,10 @@
 import { logger } from '@chainlink/gauntlet-core/dist/utils'
 import { AddressBook } from './addressBook'
 import { assertions } from '@chainlink/gauntlet-core/dist/utils'
-
-export class TerraLogger {
-  addressBook: AddressBook
-
-  withAddressBook(addressBook: AddressBook) {
-    this.addressBook = addressBook
-  }
-
-  // Example:  logger.info(`Destination address is ${logger.styleAddress(address)}`)
-  styleAddress(address: string): string {
-    if (!this.addressBook) {
-      logger.warn(`TerraLogger: styleAddress called before calling withAddressBook!`)
-      return address
-    }
-
-    if (this.addressBook.instances.has(address)) {
-      const name = this.addressBook.instances.get(address).name
-      if (name == 'multisig') {
-        return logger.formatMultisig(address, name)
-      } else {
-        return logger.formatContract(address, name)
-      }
-    } else if (address == this.addressBook.operator) {
-      return logger.formatOperator(address)
-    } else {
-      return address
-    }
-  }
-}
+import { Logger } from '@chainlink/gauntlet-core/dist/utils/logger'
 
 // TODO: instatntiate in terra-gauntlet-contracts instead of here?
-const terraLogger = new TerraLogger()
+const terraLogger = new Logger()
 
 export default {
   styleAddress: (address: string) => terraLogger.styleAddress(address),
