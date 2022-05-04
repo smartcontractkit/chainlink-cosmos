2507360923c587475bd3b603fb66aef860824bc1
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2022-05-04 13:43:16 +0200

    add inspect operators command

--- packages-ts/gauntlet-terra-contracts/src/lib/utils.ts
+++ packages-ts/gauntlet-terra-contracts/src/lib/utils.ts
@@ -1,25 +1,6 @@
-import { bech32 } from 'bech32'
-import { AccAddress, LCDClient } from '@terra-money/terra.js'
-
 // TODO: This function should be transfered to gauntlet-core repo.
 export function dateFromUnix(unixTimestamp: number): Date {
   return new Date(unixTimestamp * 1000)
 }
 
-export const getLatestContractEvent = async (provider: LCDClient, event: string, contract: AccAddress) => {
-  let transmissionTx = (
-    await provider.tx.search({
-      events: [
-        {
-          key: `${event}.contract_address`,
-          value: contract,
-        },
-      ],
-      'pagination.limit': '1',
-      order_by: 'ORDER_BY_DESC',
-    })
-  ).txs
-
-  return transmissionTx[0]?.logs?.[0].eventsByType[event]
-}
 export const hexToBase64 = (s: string): string => Buffer.from(s, 'hex').toString('base64')
