bd36070a0b4c35e0377e71ca59fb328a5f5d8020
Aaron Lu <50029043+aalu1418@users.noreply.github.com>
2022-06-10 17:35:32 -0400

    bump terra core (#298)
    * bump terra core
    
    * fix sha
    
    * skip integration tests for now

--- terrad.nix
+++ terrad.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "terrad-${version}";
-  version = "0.5.14";
+  version = "2.0.1";
 
   src = fetchurl {
     url = "https://github.com/terra-money/core/releases/download/v${version}/terra_${version}_Linux_x86_64.tar.gz";
-    sha256 = "sha256-FBomyS6B8WTNYDk+WnxC8KkYXs7pLEO9UGClESHQxL4=";
+    sha256 = "sha256-6uI6rVmzbqKtqlQadmKmEZVpJ50ubEAT497ulukmOzA=";
   };
 
   # nativeBuildInputs = [
