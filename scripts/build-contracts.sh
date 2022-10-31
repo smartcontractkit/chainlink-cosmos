f526a6b6a9d922a8dac08d1cec429b4a842d9b11
Blaž Hrastnik <blaz@mxxn.io>
2022-10-31 13:40:55 +0900

    Bump workspace-optimizer

--- scripts/build-contracts.sh
+++ scripts/build-contracts.sh
@@ -2,6 +2,6 @@
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.4
+  cosmwasm/workspace-optimizer:0.12.9
 # regenerate schemas
 for d in contracts/*/; do     cd "$d"; cargo schema; cd ../..; done
