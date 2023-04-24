20550926ba07380a65a6b0c15d2950be21850c72
Jordan Krage <jmank88@gmail.com>
2023-04-24 13:44:32 -0500

    use common ChainSet interface (#329)

--- pkg/cosmos/adapters/chain.go
+++ pkg/cosmos/adapters/chain.go
@@ -1,22 +1,16 @@
 package adapters
 
 import (
-	"context"
-
 	"github.com/smartcontractkit/chainlink-relay/pkg/types"
 
 	"github.com/smartcontractkit/chainlink-cosmos/pkg/cosmos/client"
 	"github.com/smartcontractkit/chainlink-cosmos/pkg/cosmos/config"
 )
 
-type ChainSet interface {
-	types.Service
-	// Chain returns chain for the given id.
-	Chain(ctx context.Context, id string) (Chain, error)
-}
+type ChainSet = types.ChainSet[string, Chain]
 
 type Chain interface {
-	types.Service
+	types.ChainService
 
 	ID() string
 	Config() config.Config
