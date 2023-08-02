34b7df737619e38c3449be925ee919c672e775e7
Calvin Wang <calvin.wang@smartcontract.com>
2023-08-02 13:28:33 +1000

    bump optimizer and wasmd version

--- scripts/build-contracts.sh
+++ scripts/build-contracts.sh
@@ -2,6 +2,6 @@
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.13.0
+  cosmwasm/workspace-optimizer:0.14.0
 # regenerate schemas
 for d in contracts/*/; do     cd "$d"; cargo schema; cd ../..; done
