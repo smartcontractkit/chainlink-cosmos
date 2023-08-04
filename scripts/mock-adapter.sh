b28af07c029d07f614aab952dd8e783e6e537f63
cfal <cfal@users.noreply.github.com>
2023-08-04 17:15:37 +0800

    add mock-adapter scripts

---
+++ scripts/mock-adapter.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cache_path="$(git rev-parse --show-toplevel)/.local-mock-server"
+binary_name="dummy-external-adapter"
+binary_path="${cache_path}/bin/${binary_name}"
+
+bash "$(dirname -- "$0")/mock-adapter-down.sh"
+
+listen_address="127.0.0.1:6060"
+if [ $# -gt 0 ]; then
+	listen_address="$1"
+fi
+echo "Listen address: ${listen_address}"
+
+if [ ! -f "${binary_path}" ]; then
+	echo "Installing mock-adapter"
+	export GOPATH="${cache_path}"
+	export GOBIN="${cache_path}/bin"
+	go install 'github.com/smartcontractkit/dummy-external-adapter@latest'
+fi
+
+nohup "${binary_path}" "${listen_address}" &>/dev/null &
+echo "Started mock-adapter (PID $!)"
