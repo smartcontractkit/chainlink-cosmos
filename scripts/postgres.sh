988bef88239e1822cb5db3742e52b286de4defd1
cfal <cfal@users.noreply.github.com>
2023-08-09 11:22:17 +0800

    scripts/postgres.sh: fetch docker ip

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -7,9 +7,15 @@ set -euo pipefail
 
 bash "$(dirname -- "$0")/postgres.down.sh"
 
+docker_ip=$(docker network inspect bridge -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}')
+if [ -z "${docker_ip}" ]; then
+	echo "Could not fetch docker ip."
+	exit 1
+fi
+
 echo "Starting postgres container"
 docker run \
-	-p 172.17.0.1:5432:5432 \
+	-p "${docker_ip}:5432:5432" \
 	-d \
 	--name "${container_name}" \
 	-e POSTGRES_USER=postgres \
