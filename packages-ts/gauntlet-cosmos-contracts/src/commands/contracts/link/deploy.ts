f3fbaa8e66f56bdbe7d71ad11f71ae9671cbca2a
cfal <cfal@users.noreply.github.com>
2023-08-11 18:47:53 +0800

    packages-ts/gauntlet-cosmos-contracts/commands/contracts: prettier

--- packages-ts/gauntlet-cosmos-contracts/src/commands/contracts/link/deploy.ts
+++ packages-ts/gauntlet-cosmos-contracts/src/commands/contracts/link/deploy.ts
@@ -25,8 +25,8 @@ export default class DeployLink extends CosmosCommand {
 
   execute = async () => {
     await prompt(`Begin deploying LINK Token?`)
-    const codeId = this.codeIds['cw20_base'] || CW20_BASE_CODE_IDs[this.flags.network];
-    logger.info('Using code ID: ' + codeId);
+    const codeId = this.codeIds['cw20_base'] || CW20_BASE_CODE_IDs[this.flags.network]
+    logger.info('Using code ID: ' + codeId)
     const deploy = await this.deploy(codeId, {
       name: 'ChainLink Token',
       symbol: 'LINK',
