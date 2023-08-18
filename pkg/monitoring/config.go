d6fc4427b8afa3a703072dbb2ca51812fff34f0e
Calvin Wang <calvin.wang@smartcontract.com>
2023-08-18 16:50:49 +1000

    remove fcdurl from config

--- pkg/monitoring/config.go
+++ pkg/monitoring/config.go
@@ -15,7 +15,6 @@ import (
 type CosmosConfig struct {
 	TendermintURL        string
 	TendermintReqsPerSec int
-	FCDURL               string
 	FCDReqsPerSec        int
 	NetworkName          string
 	NetworkID            string
@@ -79,9 +78,6 @@ func parseEnvVars(cfg *CosmosConfig) error {
 		}
 		cfg.TendermintReqsPerSec = rps
 	}
-	if value, isPresent := os.LookupEnv("COSMOS_FCD_URL"); isPresent {
-		cfg.FCDURL = value
-	}
 	if value, isPresent := os.LookupEnv("COSMOS_FCD_REQS_PER_SEC"); isPresent {
 		rps, err := strconv.Atoi(value)
 		if err != nil {
@@ -126,7 +122,6 @@ func validateConfig(cfg CosmosConfig) error {
 	// Required config
 	for envVarName, currentValue := range map[string]string{
 		"COSMOS_TENDERMINT_URL": cfg.TendermintURL,
-		"COSMOS_FCD_URL":        cfg.FCDURL,
 		"COSMOS_NETWORK_NAME":   cfg.NetworkName,
 		"COSMOS_NETWORK_ID":     cfg.NetworkID,
 		"COSMOS_CHAIN_ID":       cfg.ChainID,
@@ -138,7 +133,6 @@ func validateConfig(cfg CosmosConfig) error {
 	// Validate URLs.
 	for envVarName, currentValue := range map[string]string{
 		"COSMOS_TENDERMINT_URL": cfg.TendermintURL,
-		"COSMOS_FCD_URL":        cfg.FCDURL,
 	} {
 		if _, err := url.ParseRequestURI(currentValue); currentValue != "" && err != nil {
 			return fmt.Errorf("%s='%s' is not a valid URL: %w", envVarName, currentValue, err)
@@ -150,9 +144,6 @@ func validateConfig(cfg CosmosConfig) error {
 func applyDefaults(cfg *CosmosConfig) {
 	if cfg.TendermintReqsPerSec == 0 {
 		cfg.TendermintReqsPerSec = 1
-	}
-	if cfg.FCDURL == "" {
-		cfg.FCDURL = "https://fcd.terra.dev/"
 	}
 	if cfg.FCDReqsPerSec == 0 {
 		cfg.FCDReqsPerSec = 1
