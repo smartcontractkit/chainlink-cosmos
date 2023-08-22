dafe42086c12265e1f375190661b1cfd4b7b7550
Chia Yong Kang <chiayongkang@hotmail.com>
2023-08-22 10:58:26 +0800

    [BCI-1635] Integrate Generic Fixed Fee Estimator (#350)
    Co-authored-by: Calvin Wang <calvin.wang@smartcontract.com>

--- pkg/cosmos/client/gas_price_estimator_test.go
+++ pkg/cosmos/client/gas_price_estimator_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/smartcontractkit/chainlink-relay/pkg/logger"
+
 	"go.uber.org/zap"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -15,6 +16,7 @@ import (
 
 func TestGasPriceEstimators(t *testing.T) {
 	lggr, logs := logger.TestObserved(t, zap.WarnLevel)
+	sugaredLggr := logger.Sugared(lggr)
 	assertLogsLen := func(t *testing.T, l int) func() {
 		return func() { assert.Len(t, logs.TakeAll(), l) }
 	}
@@ -22,7 +24,7 @@ func TestGasPriceEstimators(t *testing.T) {
 	t.Run("fixed", func(t *testing.T) {
 		gpeFixed := NewFixedGasPriceEstimator(map[string]sdk.DecCoin{
 			"ucosm": sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("10")),
-		})
+		}, sugaredLggr)
 		p, err := gpeFixed.GasPrices()
 		require.NoError(t, err)
 		price, ok := p["ucosm"]
@@ -87,7 +89,7 @@ func TestGasPriceEstimators(t *testing.T) {
 		cachingGpe := NewCachingGasPriceEstimator(closureGpe, lggr)
 		gpeFixed := NewFixedGasPriceEstimator(map[string]sdk.DecCoin{
 			"ucosm": sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("10")),
-		})
+		}, sugaredLggr)
 		gpe := NewMustGasPriceEstimator([]GasPricesEstimator{cachingGpe, gpeFixed}, lggr)
 		t.Cleanup(assertLogsLen(t, 1))
 		fixedPrices := gpe.GasPrices()
@@ -100,5 +102,58 @@ func TestGasPriceEstimators(t *testing.T) {
 		ucosm, ok = gpePrices["ucosm"]
 		assert.True(t, ok)
 		assert.NotEqual(t, "10.000000000000000000", ucosm.Amount.String())
+	})
+}
+
+func TestFixedPriceGasEstimator(t *testing.T) {
+	lggr := logger.Sugared(logger.Test(t))
+
+	t.Run("bump gas price", func(t *testing.T) {
+		tests := []struct {
+			name             string
+			currentGasPrice  sdk.DecCoin
+			originalGasPrice sdk.DecCoin
+			maxGasPrice      sdk.DecCoin
+			maxBumpPrice     sdk.DecCoin
+			minBumpPrice     sdk.DecCoin
+			bumpPercent      uint16
+			want             sdk.DecCoin
+		}{
+			{
+				name:             "Bump the gas price by minimum as bumpPercent is less than bumpMin",
+				currentGasPrice:  sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.001")),
+				originalGasPrice: sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.001")),
+				maxGasPrice:      sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.02")),
+				maxBumpPrice:     sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.3")),
+				minBumpPrice:     sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.0005")),
+				bumpPercent:      30,
+				want:             sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.0015")),
+			},
+			{
+				name:             "Bump the gas price by 30% as bumpPercent is greater than bumpMin",
+				currentGasPrice:  sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.0010")),
+				originalGasPrice: sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.001")),
+				maxGasPrice:      sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.02")),
+				maxBumpPrice:     sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.3")),
+				minBumpPrice:     sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.00005")),
+				bumpPercent:      30,
+				want:             sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.0013")),
+			},
+		}
+
+		gpeFixed := NewFixedGasPriceEstimator(map[string]sdk.DecCoin{
+			"ucosm": sdk.NewDecCoinFromDec("ucosm", sdk.MustNewDecFromStr("0.001")),
+		}, lggr)
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				bumpedGasPrice, err := gpeFixed.CalculateBumpGasPrice("ucosm", tt.currentGasPrice, tt.originalGasPrice, tt.maxGasPrice, tt.maxBumpPrice, tt.minBumpPrice, tt.bumpPercent)
+				require.NoError(t, err)
+				gpeFixed.gasPrices["ucosm"] = bumpedGasPrice
+				actualGasPrice, err := gpeFixed.GasPrice("ucosm")
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, actualGasPrice)
+			})
+		}
 	})
 }
