696506fa7487ad393b84ab78d34ac4af110fbf9e
Calvin Wang <calvin.wang@smartcontract.com>
2023-09-07 14:20:34 +1000

    add hardcoded bech32prefix and feetoken to integration tests

--- integration-tests/common/common.go
+++ integration-tests/common/common.go
@@ -127,6 +127,8 @@ func NewCommon(t *testing.T) *Common {
 	chainlinkConfig := fmt.Sprintf(`[[Cosmos]]
 Enabled = true
 ChainID = '%s'
+Bech32Prefix = 'wasm'
+FeeToken = 'ucosm'
 [[Cosmos.Nodes]]
 Name = 'primary'
 TendermintURL = '%s'
