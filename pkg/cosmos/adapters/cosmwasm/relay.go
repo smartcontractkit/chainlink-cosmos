4e88e371e78a052dd1dcae0110f5c8b4a33a4114
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2023-10-17 17:34:59 -0700

    Add unimplemented ChainReader() method to MedianProvider

--- pkg/cosmos/adapters/cosmwasm/relay.go
+++ pkg/cosmos/adapters/cosmwasm/relay.go
@@ -144,3 +144,7 @@ func (p *medianProvider) MedianContract() median.MedianContract {
 func (p *medianProvider) OnchainConfigCodec() median.OnchainConfigCodec {
 	return median.StandardOnchainConfigCodec{}
 }
+
+func (p *medianProvider) ChainReader() relaytypes.ChainReader {
+	return nil
+}
