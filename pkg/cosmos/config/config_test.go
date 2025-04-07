6f5ddcd08cb1e44b5877b70d8a50a57d62662578
Jordan Krage <jmank88@gmail.com>
2025-04-07 08:11:29 -0500

    pkg/cosmos/config: config enhancements

--- pkg/cosmos/config/config_test.go
+++ pkg/cosmos/config/config_test.go
@@ -1,14 +1,17 @@
 package config
 
 import (
+	_ "embed"
 	"reflect"
 	"testing"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/config"
+	"github.com/smartcontractkit/chainlink-common/pkg/config/configtest"
 
 	"github.com/smartcontractkit/chainlink-cosmos/pkg/cosmos/db"
 )
@@ -93,4 +96,42 @@ func TestCosmosConfig_GetNode(t *testing.T) {
 
 func ptr[T any](t T) *T {
 	return &t
+}
+
+func TestDefaults_fieldsNotNil(t *testing.T) {
+	configtest.AssertFieldsNotNil(t, Defaults())
+}
+
+func TestDocsTOMLComplete(t *testing.T) {
+	configtest.AssertDocsTOMLComplete[TOMLConfig](t, docsTOML)
+}
+
+//go:embed testdata/config-full.toml
+var fullTOML string
+
+func TestTOMLConfig_FullMarshal(t *testing.T) {
+	full := TOMLConfig{
+		ChainID: ptr("Ibiza-808"),
+		Enabled: ptr(true),
+		Chain: Chain{
+			Bech32Prefix:         ptr("wasm"),
+			BlockRate:            config.MustNewDuration(time.Minute),
+			BlocksUntilTxTimeout: ptr[int64](12),
+			ConfirmPollPeriod:    config.MustNewDuration(time.Second),
+			FallbackGasPrice:     ptr(decimal.RequireFromString("0.001")),
+			GasToken:             ptr("ucosm"),
+			GasLimitMultiplier:   ptr(decimal.RequireFromString("1.2")),
+			MaxMsgsPerBatch:      ptr[int64](17),
+			OCR2CachePollPeriod:  config.MustNewDuration(time.Minute),
+			OCR2CacheTTL:         config.MustNewDuration(time.Hour),
+			TxMsgTimeout:         config.MustNewDuration(time.Second),
+		},
+		Nodes: []*Node{
+			{
+				Name:          ptr("primary"),
+				TendermintURL: config.MustParseURL("http://columbus.cosmos.com"),
+			},
+		},
+	}
+	configtest.AssertFullMarshal(t, full, fullTOML)
 }
