7a6acf94ac618e8786057ab6d38231a1a8eb27b6
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2025-04-14 12:25:41 -0500

    fix ocr2 test for latest cosmos

--- integration-tests/ocr2_test.go
+++ integration-tests/ocr2_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 	"time"
 
+	"cosmossdk.io/math"
 	relaylogger "github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
 
@@ -62,8 +63,8 @@ func TestOCRBasic(t *testing.T) {
 	require.NoError(t, err, "Could not create private key from mnemonic")
 	logger.Info().Str("from", testAccount.String()).Msg("Funding nodes")
 
-	gasPrice := types.NewDecCoinFromDec("ucosm", types.MustNewDecFromStr("1"))
-	amount := []types.Coin{types.NewCoin("ucosm", types.NewInt(int64(10000000)))}
+	gasPrice := types.NewDecCoin("ucosm", math.NewInt(int64(10000000)))
+	amount := []types.Coin{types.NewCoin("ucosm", math.NewInt(int64(10000000)))}
 	accountNumber, sequenceNumber, err := cosmosClient.Account(ctx, testAccount)
 	require.NoError(t, err, "Could not get account")
 
